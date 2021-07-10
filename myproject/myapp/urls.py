3fc983421fefc89d89448557867d8f1a5a06c50a
JinKim1221 <sdo7329@gmail.com>
2021-07-10 17:37:22 +0100

    create view to add bookmark

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookmarkListView
+from .views import BookmarkListView, BookmarkCreateView
 
 urlpatterns = [
     # http://127.0.0.1/myapp/????
     # ?????
-    path("", BookmarkListView.as_view(), name='List'),
+    path("", BookmarkListView.as_view(), name='list'),
+    path("add/", BookmarkCreateView.as_view(), name="Add")
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -10,6 +10,14 @@ from django.shortcuts import render
 # access webpage -> see the webpage
 # input URL -> Server looks for View -> Response
 from django.views.generic.list import ListView
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 class BookmarkListView(ListView) :
     model = Bookmark
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    # ImproperlyConfigured at /myapp/add/ : Fields are needed
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'

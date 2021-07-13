d48f8238acebf09c46c7b08043601c5642ea9707
JinKim1221 <sdo7329@gmail.com>
2021-07-13 17:05:31 +0100

    deploy

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zvr&b+^5tz88p7upfgeu&d44#8u%-+t73&k)#!w(^mvt4k%njz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
